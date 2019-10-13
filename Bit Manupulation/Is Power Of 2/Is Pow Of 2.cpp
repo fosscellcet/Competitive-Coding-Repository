b99923c86e8aab17a12fae4f0f53ea829bbe1ab2
Yashasvi Goel <31849052+yashasvi-goel@users.noreply.github.com>
2019-10-13 18:24:49 +0530

    Update Is Pow Of 2.cpp

--- Bit Manupulation/Is Power Of 2/Is Pow Of 2.cpp
+++ Bit Manupulation/Is Power Of 2/Is Pow Of 2.cpp
@@ -2,13 +2,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-bool isPow2(int x ){
-  return(!(x&(x-1)));
+bool isPow2(int x){
+  return(!(x&(x-1)));//(100)&(011)=0; (111)&(110)!=0;we return !(result); hence we get 1 when we have a power of two;
 }
 
 int main(){
-int n;
-cin>>n;
-cout<<isPow2(n);
+  int n;
+  cin>>n;
+  cout<<isPow2(n);
 }
