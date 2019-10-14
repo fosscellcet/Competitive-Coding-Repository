317762173b30f946c42db5092b81c4a5e1d25b49
nandakishormpai <52309081+nandakishormpai@users.noreply.github.com>
2019-10-14 20:19:00 +0530

    quick sort.cpp
    i have uploaded code of quick sort which use divide and conquer rule

--- Sorting/Bubble Sort/bubblesort.cpp
+++ Sorting/Bubble Sort/bubblesort.cpp
@@ -1,43 +1,67 @@
-// C++ program for implementation of Bubble sort 
-#include <bits/stdc++.h> 
-using namespace std; 
-
-void swap(int *xp, int *yp) 
+/* C implementation QuickSort */
+#include<stdio.h> 
+void swap(int* a, int* b) 
 { 
-	int temp = *xp; 
-	*xp = *yp; 
-	*yp = temp; 
+	int t = *a; 
+	*a = *b; 
+	*b = t; 
 } 
 
-// A function to implement bubble sort 
-void bubbleSort(int arr[], int n) 
+
+int partition (int arr[], int low, int high) 
 { 
-	int i, j; 
-	for (i = 0; i < n-1; i++)	 
-	
-	// Last i elements are already in place 
-	for (j = 0; j < n-i-1; j++) 
-		if (arr[j] > arr[j+1]) 
-			swap(&arr[j], &arr[j+1]); 
+	int pivot = arr[high]; 
+	int i = (low - 1); 
+
+	for (int j = low; j <= high- 1; j++) 
+	{ 
+		// If current element is smaller than or 
+		// equal to pivot 
+		if (arr[j] <= pivot) 
+		{ 
+			i++; // increment index of smaller element 
+			swap(&arr[i], &arr[j]); 
+		} 
+	} 
+	swap(&arr[i + 1], &arr[high]); 
+	return (i + 1); 
 } 
 
-/* Function to print an array */
+
+void quickSort(int arr[], int low, int high) 
+{ 
+	if (low < high) 
+	{ 
+		/* pi is partitioning index, arr[p] is now 
+		at right place */
+		int pi = partition(arr, low, high); 
+
+		// Separately sort elements before 
+		// partition and after partition 
+		quickSort(arr, low, pi - 1); 
+		quickSort(arr, pi + 1, high); 
+	} 
+} 
+
+
 void printArray(int arr[], int size) 
 { 
 	int i; 
-	for (i = 0; i < size; i++) 
-		cout << arr[i] << " "; 
-	cout << endl; 
+	for (i=0; i < size; i++) 
+		printf("%d ", arr[i]); 
+	printf("n"); 
 } 
 
-// Driver code 
+
 int main() 
 { 
-	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+	int arr[20]; 
+	cin<<arr;
 	int n = sizeof(arr)/sizeof(arr[0]); 
-	bubbleSort(arr, n); 
-	cout<<"Sorted array: \n"; 
+	quickSort(arr, 0, n-1); 
+	printf("Sorted array: n"); 
 	printArray(arr, n); 
 	return 0; 
 } 
+ 
 
