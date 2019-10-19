b159fc899cd6b5b13f9f5efbcb2429da7b9b1079
Nitish Jatwar <nkjatwar@gmail.com>
2019-10-19 04:12:21 -0700

    printing index if found

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -2,17 +2,19 @@ def binary_search(list1,item):
     first = 0
     last = len(list1)-1
     f=0
+    a=0
     while( first<=last and f==0):
         mid = int((first + last)/2)
         if list1[mid] == item :
             f = 1
+            a=mid
         else:
             if item < list1[mid]:
                 last = mid - 1
             else:
                 first = mid + 1    
-    if f==0:
-        return("found")
+    if f==1:
+        return(a)
     else:
         return("not found")
         
